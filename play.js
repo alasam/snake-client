8110fa5c9c28a4961b99a2ea014333be52eb8403
Anthony <alasam@live.com>
2021-11-04 14:09:10 -0400

    Add setupInput and handleUserInput functions in play.js

--- play.js
+++ play.js
@@ -2,10 +2,18 @@
 const {connect} = require('./client');
 const net = require("net");
 
-// establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
+const handleUserInput = function () {
+  stdin.on("data", handleUserInput);
+};
 
-
-
+setupInput();
 console.log("Connecting ...");
 connect();
